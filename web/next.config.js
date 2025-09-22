3c0f6d0ae5e3d6e5e2a905fe713ada52e8634c50
Ehtiyar <63013581+Ehtiyar@users.noreply.github.com>
2025-09-23 02:01:50 +0300

    Add files via upload

--- web/next.config.js
+++ web/next.config.js
@@ -1,11 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: '**' },
       { protocol: 'http', hostname: '**' }
     ]
+  },
+  experimental: {
+    esmExternals: false
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        crypto: false,
+      }
+    }
+    return config
   }
 }
 
